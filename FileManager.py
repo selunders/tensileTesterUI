bc4e17c2e012b75fb17bd860425e1cfac977027c
selunders <sethlunders@gmail.com>
2023-01-19 16:35:01 -0800

    Removed class from FileManager

--- FileManager.py
+++ FileManager.py
@@ -1,49 +1,46 @@
 import dearpygui.dearpygui as dpg
 import os
 
-class FileManager:
-    # directory_set_callback = None
-    def load_directory(self, output_dir):
-            self.output_directory = output_dir
-            if os.path.exists(f"{output_dir}/params.json"):
-                self.load_params_from_file(f"{output_dir}/params.json")
-            else:
-                print("Error in function 'load_directory' in FileManager class")
+output_directory = None
 
-    def callback(self, sender, app_data):
-        # print(f"{sender} sent {app_data}")
-        # print(sender['file_path_name'])
-        output_dir = sender['file_path_name']
-        if os.path.exists(output_dir):
-            print("Directory exists! Loading information.")
-            self.load_directory(output_dir)
+def load_directory(output_dir):
+        output_directory = output_dir
+        if os.path.exists(f"{output_dir}/params.json"):
+            load_params_from_file(f"{output_dir}/params.json")
         else:
-            os.mkdir(output_dir)
-            print(f"Creating directory {output_dir}")
-            self.load_directory(output_dir)
+            print("Error in function 'load_directory' in FileManager class")
 
-    def cancel_callback(sender, app_data):
-        print('Cancel clicked. Not changing directory path.')
+def callback(sender, app_data):
+    print(f"{sender} sent {app_data}")
+    # print(sender['file_path_name'])
+    output_dir = app_data['file_path_name']
+    if os.path.exists(output_dir):
+        print("Directory exists! Loading information.")
+        load_directory(output_dir)
+    else:
+        os.mkdir(output_dir)
+        print(f"Creating directory {output_dir}")
+        load_directory(output_dir)
 
-    def export_data(self, data, test_params=""):
-        if self.output_directory != "":
-            if test_params:
-                self.test_params_to_file(test_params)
-            self.data_to_npy(data)
-        else:
-            print("ERROR(FileManager.export_data): No output directory specified.")
+def cancel_callback(sender, app_data):
+    print('Cancel clicked. Not changing directory path.')
 
-    def data_to_npy(self, data):
-        print("WARNING(FileManager.data_to_npy): Not yet implemented.")
+def export_data(data, test_params=""):
+    if output_directory != "":
+        if test_params:
+            test_params_to_file(test_params)
+        data_to_npy(data)
+    else:
+        print("ERROR(FileManager.export_data): No output directory specified.")
 
-    def test_params_to_file(self, params):
-        print("WARNING(FileManager.test_params_to_file): Not yet implemented.")
-        # Write params to file.
-        # Initial thought is to put a method that stores/loads params in an object.
-        # That object is passed here, where it is written to the disk.
+def data_to_npy(self, data):
+    print("WARNING(FileManager.data_to_npy): Not yet implemented.")
 
-    def load_params_from_file(self, params):
-        print("WARNING(FileManager.load_params_from_file): Not yet implemented.")
+def test_params_to_file(self, params):
+    print("WARNING(FileManager.test_params_to_file): Not yet implemented.")
+    # Write params to file.
+    # Initial thought is to put a method that stores/loads params in an object.
+    # That object is passed here, where it is written to the disk.
 
-    def __init__(self):
-        self.output_directory = None
+def load_params_from_file(self, params):
+    print("WARNING(FileManager.load_params_from_file): Not yet implemented.")
